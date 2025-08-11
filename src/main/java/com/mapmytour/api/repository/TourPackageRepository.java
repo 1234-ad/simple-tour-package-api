c3429e79d7b6fa2bca941dfac051505dd04d46b0
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-08-11 09:57:28 +0530

    Add TourPackage repository interface

---
+++ src/main/java/com/mapmytour/api/repository/TourPackageRepository.java
@@ -0,0 +1,9 @@
+package com.mapmytour.api.repository;
+
+import com.mapmytour.api.entity.TourPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TourPackageRepository extends JpaRepository<TourPackage, Long> {
+}
