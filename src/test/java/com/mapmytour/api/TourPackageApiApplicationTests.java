b663fd68250542eff20e6aeb7c8ea66795ce9321
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-08-11 09:58:54 +0530

    Add basic test class

---
+++ src/test/java/com/mapmytour/api/TourPackageApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mapmytour.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPackageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
